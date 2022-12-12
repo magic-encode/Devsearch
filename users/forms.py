617d48a14bead55e793b78790f7037ed61f686dc
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2022-12-12 16:21:08 +0500

    Fixed forms

--- users/forms.py
+++ users/forms.py
@@ -15,7 +15,7 @@ class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
 
-        for name, field in self.fields.items():
+        for field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
 
 
