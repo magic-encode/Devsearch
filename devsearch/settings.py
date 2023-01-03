ba411a3306aa6254d012e96c034f486846ed3779
Abdullajon <101052604+django-dev-uz@users.noreply.github.com>
2023-01-03 16:46:27 +0500

    Fixed another files

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -114,25 +114,20 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': env.str('DB_ENGINE'),
-#         'NAME': env.str('DB_NAME'),
-#         'USER': env.str('DB_USER'),
-#         'PASSWORD': env.str('DB_PASSWORD'),
-#         'HOST': env.str('DB_HOST'),
-#         'PORT': env.int('DB_PORT'),
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': env.str('DB_ENGINE'),
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.int('DB_PORT'),
     }
 }
+
+
+
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
